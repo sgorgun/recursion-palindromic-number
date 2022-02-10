d37ac4f2ffc79e9b366af4ea68cff89b26443a32
Alexander Rykov <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-10 09:40:01 +0300

    add

---
+++ PalindromicNumber/NumbersExtension.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace PalindromicNumberTask
+{
+    /// <summary>
+    /// Provides static method for working with integers.
+    /// </summary>
+    public static class NumbersExtension
+    {
+        /// <summary>
+        /// Determines if a number is a palindromic number, see https://en.wikipedia.org/wiki/Palindromic_number.
+        /// </summary>
+        /// <param name="number">Verified number.</param>
+        /// <returns>true if the verified number is palindromic number; otherwise, false.</returns>
+        /// <exception cref="ArgumentException"> Thrown when source number is less than zero. </exception>
+        public static bool IsPalindromicNumber(int number)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
